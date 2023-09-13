e062b93efbb8ed130b82135d55f36226b287ed0d
muhsin <muhsintt777@gmail.com>
2023-09-14 00:29:59 +0530

    feat : add token verify middlwware

--- src/posts/posts-routes.ts
+++ src/posts/posts-routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { PostsController } from "./posts-controller";
+import { AuthMiddleware } from "middlewares/auth-middleware";
 
 const router = Router();
 
 router.post("/", PostsController.addPost);
-router.get("/", PostsController.getAllPost);
+router.get("/", AuthMiddleware.verifyToken, PostsController.getAllPost);
 router.delete("/:id", PostsController.deletePost);
 
 export { router as postsRouter };
