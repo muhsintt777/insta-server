615701560862fca806e256b4d4a5717033c85e5e
muhsin <muhsintt777@gmail.com>
2023-09-17 18:22:16 +0530

    feat : add regex constants

--- src/configs/constants.ts
+++ src/configs/constants.ts
@@ -2,3 +2,8 @@ export const TABLES = {
   POSTS: "posts",
   USERS: "users",
 };
+
+export const REGEX = {
+  email: /^[\w\.-]+[\+\w\.-]*@[\w\.-]+\.\w+$/,
+  password: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@#$%^&+=!]).{8,}$/,
+};
