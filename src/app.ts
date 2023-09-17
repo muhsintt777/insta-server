79e637ce7539e3da89d757a686d51ee0d476d826
muhsin <muhsintt777@gmail.com>
2023-09-17 18:50:51 +0530

    feat : add cookie parser middle

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import express from "express";
 import { appRouter } from "./routes";
+import cookieParser from "cookie-parser";
+
 const app = express();
 
 app.use(express.json());
+app.use(cookieParser());
 app.use("/", appRouter);
 
 export { app };
