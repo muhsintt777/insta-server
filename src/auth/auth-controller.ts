28f47395be88e2602751e9fd881bb4eda85961bb
muhsin <muhsintt777@gmail.com>
2023-09-17 18:22:35 +0530

    feat : add loginreq validation

--- src/auth/auth-controller.ts
+++ src/auth/auth-controller.ts
@@ -1,19 +1,15 @@
 import { Request, Response } from "express";
 import { AuthService } from "./auth-service";
+import { AuthValidation } from "./auth-validation";
 
 export class AuthController {
   static async login(req: Request, res: Response) {
     try {
-      const email: string | null = req.body.email || null;
-      const password: string | null = req.body.password || null;
-      if (!email || !password) {
-        throw {
-          statusCode: 400,
-          errorMessage: "email and password are required",
-        };
-      }
-
-      const accessToken = await AuthService.login(email, password);
+      const userCreds = AuthValidation.loginReq(req.body);
+      const accessToken = await AuthService.login(
+        userCreds.email,
+        userCreds.password
+      );
       res.status(200).json({ accessToken });
     } catch (err) {
       console.log(err);
