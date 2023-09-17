1d1d87b3aecaae4adccc8335d0ae64df1a30ea88
muhsin <muhsintt777@gmail.com>
2023-09-17 18:29:46 +0530

    fix : login req schema trim

--- src/auth/auth-schema.ts
+++ src/auth/auth-schema.ts
@@ -14,6 +14,7 @@ export const LoginReqSchema = z.object({
       required_error: "Password is required",
       invalid_type_error: "Password must be string",
     })
+    .trim()
     .regex(REGEX.password, "Password is not valid"),
 });
 export type LoginReqType = z.infer<typeof LoginReqSchema>;
