1f0e8a2dd5318fb00f4e74fbed55bccdbea2bb62
muhsin <muhsintt777@gmail.com>
2023-09-17 18:21:27 +0530

    chore : create auth schema

---
+++ src/auth/auth-schema.ts
@@ -0,0 +1,19 @@
+import { z } from "zod";
+import { REGEX } from "configs/constants";
+
+export const LoginReqSchema = z.object({
+  email: z
+    .string({
+      required_error: "Email is required",
+      invalid_type_error: "Email must be string",
+    })
+    .trim()
+    .regex(REGEX.email, "Email is not valid"),
+  password: z
+    .string({
+      required_error: "Password is required",
+      invalid_type_error: "Password must be string",
+    })
+    .regex(REGEX.password, "Password is not valid"),
+});
+export type LoginReqType = z.infer<typeof LoginReqSchema>;
