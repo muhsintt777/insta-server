d9b5924670a43b15446421ceb6f6c97267d42214
muhsin <muhsintt777@gmail.com>
2023-09-17 18:51:23 +0530

    feat : add cookie age

--- src/configs/constants.ts
+++ src/configs/constants.ts
@@ -7,3 +7,5 @@ export const REGEX = {
   email: /^[\w\.-]+[\+\w\.-]*@[\w\.-]+\.\w+$/,
   password: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@#$%^&+=!]).{8,}$/,
 };
+
+export const TOKEN_COOKIE_AGE = 2 * 60 * 1000;
