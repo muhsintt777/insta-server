3ebeefeba237ea7e457f282fea35bfefd1c06001
muhsin <muhsintt777@gmail.com>
2023-09-18 19:04:41 +0530

    feat : add get user

--- src/users/user-service.ts
+++ src/users/user-service.ts
@@ -3,24 +3,42 @@ import { UserModal } from "./user-modal";
 import { User } from "./user";
 
 export class UserService {
-  static async getUser() {}
+  static async getUser(id: number): Promise<Omit<User, "password" | "email">> {
+    const userArr = await UserModal.findAll({
+      where: {
+        id: id,
+      },
+    });
+    if (!userArr.length)
+      throw { statusCode: 404, errorMessage: "User not found" };
+
+    const user = userArr[0]?.toJSON();
+    delete user.password;
+    delete user.email;
+    const mutatedUser: Omit<User, "password" | "email"> = user;
+    return mutatedUser;
+  }
 
   static async createUser(
     firstName: string,
     lastName: string | null | undefined,
     email: string,
     password: string
-  ): Promise<User> {
+  ): Promise<Omit<User, "password">> {
     try {
       const hashedPwd = await Crypto.hashString(password, 10);
-      const user = await UserModal.create({
+      const userObj = await UserModal.create({
         firstName,
         lastName,
         email,
         password: hashedPwd,
       });
+      const user = userObj.toJSON();
+      delete user.password;
+      delete user.email;
+      const userWithoutPassword: Omit<User, "password"> = user;
 
-      return user.toJSON() as User;
+      return userWithoutPassword;
     } catch (err) {
       if (err.name === "SequelizeUniqueConstraintError") {
         throw {
