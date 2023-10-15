5f0553d562125873bc743845d1a610b0940ecfd0
muhsin <muhsintt777@gmail.com>
2023-10-15 15:58:42 +0530

    feat : add filetype constant

--- src/configs/constants.ts
+++ src/configs/constants.ts
@@ -10,3 +10,9 @@ export const REGEX = {
 };
 
 export const TOKEN_COOKIE_AGE = 24 * 60 * 60 * 1000;
+
+export const FILE_TYPE = {
+  imagePNG: "image/png",
+  imageJPEG: "image/jpeg",
+  imageJPG: "image/jpg",
+};
