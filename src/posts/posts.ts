f589b52ba17377c3a5ee3baaffa1fd48b8144bc5
muhsin <muhsintt777@gmail.com>
2023-10-15 17:48:07 +0530

    feat : add posts column type

--- src/posts/posts.ts
+++ src/posts/posts.ts
@@ -6,6 +6,40 @@ export interface Post {
   caption: string;
   likeCount: number;
   commentCount: number;
+  status: 1 | 2;
 }
 
 export interface AddPostParams {}
+
+interface ColStatus {
+  name: "status";
+  value: 1 | 2;
+}
+
+interface ColImageUrl {
+  name: "image_url";
+  value: string;
+}
+
+interface ColCaption {
+  name: "caption";
+  value: string;
+}
+
+interface ColLikeCount {
+  name: "like_count";
+  value: number;
+}
+
+interface ColCommentCount {
+  name: "comment_count";
+  value: number;
+}
+
+export type PostsColumn = (
+  | ColStatus
+  | ColCaption
+  | ColImageUrl
+  | ColLikeCount
+  | ColCommentCount
+)[];
