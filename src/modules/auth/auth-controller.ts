e62af8ac24a1d067b824a633ac507c602c7ca0ad
Muhsin <muhsintt777@gmail.com>
2024-07-21 18:42:18 +0530

    fix : ts build error

--- src/modules/auth/auth-controller.ts
+++ src/modules/auth/auth-controller.ts
@@ -1,17 +1,16 @@
 import { Request, Response } from "express";
-import { TOKEN_COOKIE_AGE } from "configs/constants";
-import { AuthService } from "./auth-service";
-import { AuthValidation } from "./auth-validation";
+// import { TOKEN_COOKIE_AGE } from "configs/constants";
+// import { AuthService } from "./auth-service";
+// import { AuthValidation } from "./auth-validation";
 
 export class AuthController {
-  static async login(req: Request, res: Response) {
+  static async login(_req: Request, res: Response) {
     try {
-      const userCreds = AuthValidation.loginReq(req.body);
+      // const userCreds = AuthValidation.loginReq(req.body);
       // const accessToken = await AuthService.login(
       //   userCreds.email,
       //   userCreds.password
       // );
-
       // res.cookie("token", accessToken, {
       //   httpOnly: true,
       //   secure: true,
@@ -28,10 +27,9 @@ export class AuthController {
     }
   }
 
-  static async getUser(req: Request, res: Response) {
+  static async getUser(_req: Request, res: Response) {
     try {
-      const { token } = AuthValidation.getUserReq(req.body);
-
+      // const { token } = AuthValidation.getUserReq(req.body);
       // const user = await AuthService.getUser(token.id);
       // res.status(200).json(user);
     } catch (err) {
