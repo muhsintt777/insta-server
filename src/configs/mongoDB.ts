2a288beb11b7c7d6bfa0ac3a5695b512a8cae727
Muhsin <muhsintt777@gmail.com>
2024-07-21 19:56:49 +0530

    chore : db connection oprions update

--- src/configs/mongoDB.ts
+++ src/configs/mongoDB.ts
@@ -1,14 +1,13 @@
-import { connect } from "mongoose";
+import mongoose from "mongoose";
 const MONGO_URI = process.env.MONGO_URI;
 
 export const connectDB = async () => {
   try {
     if (!MONGO_URI) throw new Error("no db url found");
-
-    const connectionInstance = await connect(MONGO_URI);
-    console.log(
-      `mongoDB connected HOST: ${connectionInstance.connection.host}`
-    );
+    await mongoose.connect(MONGO_URI, {
+      dbName: "test",
+    });
+    console.log(`mongoDB connected`);
   } catch (err) {
     console.log("mongodb connection failed", err);
     process.exit(1);
