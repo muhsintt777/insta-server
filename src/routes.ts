e6afb59fe1554ba4b738b8694dea802119074c2f
Muhsin <muhsintt777@gmail.com>
2024-10-13 23:49:57 +0530

    chore : disable unused routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
 // import { authRouter } from "modules/auth/auth-routes";
-import { postsRouter } from "modules/posts/posts-routes";
+// import { postsRouter } from "modules/posts/posts-routes";
 import { userRouter } from "modules/users/user-routes";
 
 const router = Router();
 
-router.use("/posts", postsRouter);
+// router.use("/posts", postsRouter);
 router.use("/users", userRouter);
 // router.use("/auth", authRouter);
 
