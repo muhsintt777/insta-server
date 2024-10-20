078edf132986730a37fcb15429df435c431d5d55
Muhsin <muhsintt777@gmail.com>
2024-10-20 22:11:43 +0530

    feat : add verify token functions

--- src/utils/token.ts
+++ src/utils/token.ts
@@ -1,6 +1,8 @@
 import jwt from "jsonwebtoken";
-const accessTokenKey = process.env.ACCESS_TOKEN_KEY as string;
-const refreshTokenKey = process.env.REFRESH_TOKEN_KEY as string;
+const ACCESS_TOKEN_KEY = process.env.ACCESS_TOKEN_KEY as string;
+const REFRESH_TOKEN_KEY = process.env.REFRESH_TOKEN_KEY as string;
+const REFRESH_TOKEN_EXPIRY = process.env.REFRESH_TOKEN_EXPIRY as string;
+const ACCESS_TOKEN_EXPIRY = process.env.ACCESS_TOKEN_EXPIRY as string;
 
 interface PayloadType {
   id: number;
@@ -8,10 +10,22 @@ interface PayloadType {
 
 export class Token {
   static createAccessToken(payload: PayloadType) {
-    return jwt.sign(payload, accessTokenKey, { expiresIn: "1d" });
+    return jwt.sign(payload, ACCESS_TOKEN_KEY, {
+      expiresIn: ACCESS_TOKEN_EXPIRY,
+    });
   }
 
   static createRefreshToken(payload: PayloadType) {
-    return jwt.sign(payload, refreshTokenKey, { expiresIn: "1d" });
+    return jwt.sign(payload, REFRESH_TOKEN_KEY, {
+      expiresIn: REFRESH_TOKEN_EXPIRY,
+    });
+  }
+
+  static verifyAccessToken(token: string) {
+    return jwt.verify(token, ACCESS_TOKEN_KEY);
+  }
+
+  static verifyRefreshToken(token: string) {
+    return jwt.verify(token, REFRESH_TOKEN_KEY);
   }
 }
