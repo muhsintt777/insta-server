c948cc7e279b85b99c8498eeea5f7e6e4268c1dd
Muhsin <muhsintt777@gmail.com>
2024-10-25 00:09:42 +0530

    fix : eslint warning

--- src/middlewares/file-upload-middleware.ts
+++ src/middlewares/file-upload-middleware.ts
@@ -4,7 +4,7 @@ const storage = multer.diskStorage({
   destination: function (_req, _file, cb) {
     cb(null, "./public/temp");
   },
-  filename: function (req, file, cb) {
+  filename: function (_req, file, cb) {
     // const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
     cb(null, file.originalname);
   },
