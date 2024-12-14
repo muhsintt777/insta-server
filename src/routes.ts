4a3e70f4b7cc7139df234cdf337d8548677211f1
Muhsin <muhsintt777@gmail.com>
2024-12-14 18:14:09 +0530

    feat : add health checkup route

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { healthRouter } from 'health-check/health-routes';
 import { authRouter } from 'modules/auth/auth-routes';
 // import { postsRouter } from "modules/posts/posts-routes";
 import { userRouter } from 'modules/users/user-routes';
@@ -8,5 +9,6 @@ const router = Router();
 // router.use("/posts", postsRouter);
 router.use('/users', userRouter);
 router.use('/auth', authRouter);
+router.use('/health', healthRouter);
 
 export { router as appRouter };
