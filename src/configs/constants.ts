5fe59f921e6601d684795c51e09a0160906b6724
Muhsin <muhsintt777@gmail.com>
2024-12-30 17:05:20 +0530

    chore : code clean

--- src/configs/constants.ts
+++ src/configs/constants.ts
@@ -6,8 +6,6 @@ export const REGEX = {
 } as const;
 
 export const COOKIE_EXPIRY_IN_MS = 1000 * 60 * 60 * 24 * 7; // 7 days
-export const ACCESS_TOKEN_EXPIRY_IN_MINUTES = 30;
-export const REFRESH_TOKEN_EXPIRY_IN_MINUTES = 60 * 24 * 1; // 1 day
 
 export const FILE_TYPE = {
   imagePNG: 'image/png',
