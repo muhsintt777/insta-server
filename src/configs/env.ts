bd0db9e391ce7a339f3e757bf257ec8a5b489718
Muhsin <muhsintt777@gmail.com>
2025-05-17 12:17:26 +0530

    chore : clean configs

--- src/configs/env.ts
+++ src/configs/env.ts
@@ -2,14 +2,11 @@ export const ENV = {
   SERVER_TYPE: process.env.SERVER_TYPE as 'DEV' | 'PROD',
   PORT: process.env.PORT as string,
   MONGO_URI: process.env.MONGO_URI as string,
-  CLOUDINARY_NAME: process.env.CLOUDINARY_NAME as string,
-  CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY as string,
-  CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET as string,
   ACCESS_TOKEN_KEY: process.env.ACCESS_TOKEN_KEY as string,
-  ACCESS_TOKEN_EXPIRY: process.env.ACCESS_TOKEN_EXPIRY as string,
+  ACCESS_TOKEN_EXPIRY: process.env.ACCESS_TOKEN_EXPIRY as string, //15m
   REFRESH_TOKEN_KEY: process.env.REFRESH_TOKEN_KEY as string,
-  REFRESH_TOKEN_EXPIRY: process.env.REFRESH_TOKEN_EXPIRY as string,
-  COOKIE_EXPIRY_IN_DAYS: process.env.COOKIE_EXPIRY_IN_DAYS as string,
+  REFRESH_TOKEN_EXPIRY: process.env.REFRESH_TOKEN_EXPIRY as string, //7d
+  COOKIE_EXPIRY_IN_DAYS: process.env.COOKIE_EXPIRY_IN_DAYS as string, //1
   STORAGE_BUCKET_REGION: process.env.STORAGE_BUCKET_REGION as string,
   STORAGE_BUCKET_ACC_KEY: process.env.STORAGE_BUCKET_ACC_KEY as string,
   STORAGE_BUCKET_SEC_KEY: process.env.STORAGE_BUCKET_SEC_KEY as string,
