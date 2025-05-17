cc72b0c32cfca75621cbd0dc63b1b35099d48cd3
Muhsin <muhsintt777@gmail.com>
2025-05-17 21:02:12 +0530

    chore : code clean

--- src/features/posts/posts-service.ts
+++ src/features/posts/posts-service.ts
@@ -1,11 +1,4 @@
-// import { Db } from 'configs/db';
-// import { TABLES } from 'configs/constants';
 import { Post, PostsColumn } from './posts';
-
-// interface ColStatus {
-//   name: "status";
-//   value: 1 | 2;
-// }
 
 export class PostsService {
   static async getPost(id: number) {
@@ -14,38 +7,13 @@ export class PostsService {
 
   static async getAllPost() {
     const sql = 'SELECT * FROM posts';
-    // const result = await Db.select(sql, {});
-    // if (!result) throw { statusCode: 500, errorMessage: 'DB selection failed' };
-
-    // const posts: Post[] = [];
-    // result.forEach((item) => {
-    //   const obj = {
-    //     id: item.id,
-    //     createdAt: 23424242,
-    //     updatedAt: 43534535,
-    //     imageUrl: item.image_url,
-    //     caption: item.caption,
-    //     likeCount: Number(item.like_count),
-    //     commentCount: Number(item.comment_count),
-    //   } as Post;
-    //   posts.push(obj);
-    // });
-
-    // return posts;
   }
 
   static async addPost(
     caption: string,
     imageUrl: string | null,
     status: 1 | 2,
-  ) {
-    // const sql = `INSERT INTO ${'post'} (caption, imageUrl, status) VALUES ($value1, $value2, $value3) RETURNING id`;
-    // const replacements = { value1: caption, value2: imageUrl, value3: status };
-    // const result = await Db.insert(sql, replacements);
-    // if (!result) throw { statusCode: 500, errorMessage: 'DB insert failed' };
-    // const id = result[0][0].id as number;
-    // return id;
-  }
+  ) {}
 
   static async updatePost(id: number, columns: PostsColumn) {
     const setStrings: string[] = [];
@@ -56,20 +24,10 @@ export class PostsService {
     const sql = `UPDATE ${'post'}
     SET ${setStrings.join}
     WHERE id = ${id};`;
-
-    // const result = await Db.update(sql, {});
-    // console.log(result, 'resultss update');
   }
 
   static async deletePost(id: number) {
     const sql = `DELETE FROM ${'post'} WHERE id = $id RETURNING id;`;
     const replacements = { id };
-
-    // const result = await Db.delete(sql, replacements);
-    // if (!result || !result[0] || !result[0].id)
-    //   throw { statusCode: 404, errorMessage: 'Unable to find post' };
-    // const deletedId = result[0].id as number;
-
-    // return deletedId;
   }
 }
