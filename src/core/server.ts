1f6a05832b7335c819fa41c40384ca1f5f5f06cd
Muhsin <muhsintt777@gmail.com>
2025-05-24 20:49:09 +0530

    fix : file upload path

--- src/core/server.ts
+++ src/core/server.ts
@@ -1,14 +1,24 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
 
+import { existsSync, mkdirSync } from 'fs';
 import { ENV } from 'configs/env';
-import { app } from './app';
 import { connectDB } from 'configs/db';
+import { app } from './app';
+
+const TEMP_DIR = 'public/temp';
+const port = ENV.PORT || 3500;
 
 async function startServer() {
   try {
+    // create file upload directory
+    if (!existsSync(TEMP_DIR)) {
+      mkdirSync(TEMP_DIR, { recursive: true });
+      console.log('Temporary directory created at public/temp');
+    }
+
     await connectDB();
-    const port = ENV.PORT || 3500;
+
     app.listen(port, () => {
       console.log(`Server started at ${port}`);
     });
