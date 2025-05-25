61855982a66b72c479fe64e01d972408abfd8c2e
Muhsin <muhsintt777@gmail.com>
2025-05-25 23:06:42 +0530

    feat : create json transform common config

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -12,3 +12,21 @@ export const validateId = (id: any): string => {
   if (isValidObjectId(id)) return id;
   throw new CustomError('VALIDATION_ERROR', 'Invalid ID');
 };
+
+export const getCommonJsonTransformConfig = (
+  transform?: (_doc: any, ret: any) => void,
+) => {
+  return {
+    virtuals: true,
+    versionKey: false,
+    transform: (_doc: any, ret: any) => {
+      // additional transformations
+      transform?.(_doc, ret);
+
+      // common transformations
+      ret.id = ret._id;
+      delete ret._id;
+      return ret;
+    },
+  };
+};
