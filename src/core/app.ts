8d6584e3a26494dd7892f889f58d23120ac1772b
Muhsin <muhsintt777@gmail.com>
2025-10-13 01:02:04 +0530

    chore: enable sync

--- src/core/app.ts
+++ src/core/app.ts
@@ -9,7 +9,7 @@ import { notFoundHandler } from './not-found-handler';
 import { healthHandler } from './health-handler';
 import { errorHandler } from './error-handler';
 import { metaDataHandler } from './meta-data';
-// import { syncRouter } from './sync';
+import { syncRouter } from './sync';
 
 const app = express();
 
@@ -20,7 +20,7 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 app.use('/api', appRouter);
-// app.use('/sync', syncRouter);
+app.use('/sync', syncRouter);
 app.use('/metadata', metaDataHandler);
 app.use('/health', healthHandler);
 app.use('/*', notFoundHandler);
