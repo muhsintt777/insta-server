f84384b633b1d83d8df8ed8123a16f4c08e295ed
Muhsin <muhsintt777@gmail.com>
2025-10-12 20:11:04 +0530

    chore: refactor rate limiter

--- src/middlewares/rate-limiter-middleware.ts
+++ src/middlewares/rate-limiter-middleware.ts
@@ -1,32 +1,28 @@
-import rateLimit from 'express-rate-limit';
+import rateLimit, { Options } from 'express-rate-limit';
 import { CustomError } from 'utils/error';
 
 export class RateLimitMiddleware {
-  static default = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100,
+  private static config: Partial<Options> = {
     standardHeaders: true,
     legacyHeaders: false,
     keyGenerator: (req) => req.ip ?? 'unknown',
-    handler: (_req, res) => {
+    handler: () => {
       throw new CustomError(
         'TOO_MANY_REQUESTS',
         'Too many requests, please try again later.',
       );
     },
+  };
+
+  static default = rateLimit({
+    ...this.config,
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100,
   });
 
   static auth = rateLimit({
+    ...this.config,
     windowMs: 15 * 60 * 1000, // 15 minutes
     max: 20,
-    standardHeaders: true,
-    legacyHeaders: false,
-    keyGenerator: (req) => req.ip ?? 'unknown',
-    handler: (_req, res) => {
-      throw new CustomError(
-        'TOO_MANY_REQUESTS',
-        'Too many requests, please try again later.',
-      );
-    },
   });
 }
