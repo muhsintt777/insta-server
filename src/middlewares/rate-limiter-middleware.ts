40989b8f8e062931ac7bfa489ff4578c2884025b
Muhsin <muhsintt777@gmail.com>
2025-10-15 14:10:43 +0530

    chore: config rate limit

--- src/middlewares/rate-limiter-middleware.ts
+++ src/middlewares/rate-limiter-middleware.ts
@@ -17,7 +17,7 @@ export class RateLimitMiddleware {
   static default = rateLimit({
     ...this.config,
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100,
+    max: 500,
   });
 
   static auth = rateLimit({
